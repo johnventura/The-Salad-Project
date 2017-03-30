2d184bfc58563064da51a6452df5ff9e9cec6efb
johnventura <ventura@engination.com>
2017-03-30 10:44:09 -0500

    Add files via upload

--- antisponder/destroy.c
+++ antisponder/destroy.c
@@ -1,7 +1,40 @@
+/*
+BSD 3-Clause License
+
+Copyright (c) 2017, John Ventura
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <syslog.h>
 #include <curl/curl.h>
 #include "destroy.h"
 #include "rand.h"
@@ -185,11 +218,18 @@ int destroylog(uint32_t ip, char *fakename, char **user, char **pass) {
     octets[2] = (ip >> 16) & 0xff;
     octets[3] = (ip >> 24) & 0xff;
 
-    fprintf(stdout, "Detected spoofing at %i.%i.%i.%i\n", octets[0],
+    printf("Detected spoofing at %i.%i.%i.%i\n", octets[0],
 	    octets[1], octets[2], octets[3]);
+    
+	openlog(PROGNAME, LOG_CONS | LOG_NDELAY, LOG_AUTH);
+	syslog(LOG_WARNING, "blah");
+    syslog(LOG_WARNING, "Detected spoofing at %i.%i.%i.%i\n", octets[0],
+	    octets[1], octets[2], octets[3]);
+	closelog();
 
     return (0);
 }
+
 
 int getdestroymask(char *str) {
     char *p;
